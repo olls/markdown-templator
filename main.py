d8696db7746981105163112ee6a19bfca5e0d93e
Oliver Faircliff <mail@oliverfaircliff.com>
2014-05-30 12:45:39 +0100

    Create output directory if needed

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class Templator:
         if found:
             with open(jn(self.site_d, content_f)) as f:
                 content = f.read()
-            return mk.markdown(content)
+            return mk.markdown(content, extensions=['attr_list'])
 
 
 def main():
@@ -63,8 +63,17 @@ def main():
         output_d = sys.argv[2]
     except IndexError:
         sys.exit('Usage: python main.py site_dir output_dir')
+
     for f in (site_d, output_d):
         if not os.path.isdir(f):
+
+            if f == output_d:
+                try:
+                    os.mkdir(f)
+                    continue
+                except OSError:
+                    pass
+
             sys.exit('Not a directory: ' + f)
 
     tmpltr = Templator(site_d, output_d)
